48c7f002de3a04312fd3373d5f7758d4483d7d02
nurulhusni <163105728+nurulhusni@users.noreply.github.com>
2024-03-19 21:48:11 +0900

    Add files via upload

---
+++ uli daspro2.py
@@ -0,0 +1,13 @@
+bilangan=int(input("masukan bilangan yang ingin dikonversi :\t"))
+
+konversi1=bin(bilangan)[2:]
+
+konversi2=hex(bilangan)
+
+konversi3=oct(bilangan)
+
+print("Bilangan Biner \t\t: ", konversi1.zfill(8))
+
+print("Bilangan Hexadecimal \t: ", konversi2)
+
+print("Bilangan Octal \t\t: ", konversi3)
